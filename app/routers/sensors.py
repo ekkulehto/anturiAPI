eb00acdb8f28c0f3139cdf9c12eb0cac310eeec5
Ekku Lehto <ekkulehto@proton.me>
2025-12-03 19:46:55 +0200

    feat: Add MeasurementFilter when displaying specific sensor API-endpoint

--- app/routers/sensors.py
+++ app/routers/sensors.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, status, Depends
+from typing import Annotated
+from fastapi import APIRouter, Query, status, Depends
 from sqlmodel import Session
+
+from ..schemas.filters import MeasurementFilter
 from ..database.database import get_session
 
 from ..database import sensors_crud as crud
-from ..database.models import SensorIn, SensorDb, SensorOut
+from ..database.models import SensorIn, SensorDb, SensorOut, SensorOutWithMeasurements
 
 router = APIRouter(prefix='/sensors', tags=['sensors'])
 
@@ -11,9 +14,9 @@ router = APIRouter(prefix='/sensors', tags=['sensors'])
 def get_all_sensors(*, session: Session = Depends(get_session)):
     return crud.get_all_sensors(session)
 
-@router.get('/{sensor_id}', response_model=SensorOut)
-def get_sensor_by_id(*, session: Session = Depends(get_session), sensor_id: int):
-    return crud.get_sensor_by_id(session, sensor_id)
+@router.get('/{sensor_id}', response_model=SensorOutWithMeasurements)
+def get_sensor_by_id(*, session: Session = Depends(get_session), sensor_id: int, filters: Annotated[MeasurementFilter, Query()]):
+    return crud.get_sensor_by_id(session, sensor_id, filters)
 
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=SensorDb)
 def create_sensor(*, session: Session = Depends(get_session), sensor_in: SensorIn):
