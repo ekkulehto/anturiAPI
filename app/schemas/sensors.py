9cfbc7f195aabac32df137fdb17d429a0d910e58
Ekku Lehto <ekkulehto@proton.me>
2025-12-04 20:21:38 +0200

    feat: Add new schema for sensor status update

--- app/schemas/sensors.py
+++ app/schemas/sensors.py
@@ -1,6 +1,11 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 
+from app.database.models import SensorStatus
+
 class SensorUpdate(SQLModel):
     name: Optional[str] = Field(default=None, description='New sensor name')
     segment_id: Optional[int] = Field(default=None, description='New segment ID the sensor belongs to')
+
+class SensorStatusUpdate(SQLModel):
+    status: SensorStatus
