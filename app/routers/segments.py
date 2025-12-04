04f97900b32857260b65771b75ca75fca63aa98d
Ekku Lehto <ekkulehto@proton.me>
2025-12-04 16:16:27 +0200

    feat: Add path to segment-router for updating segment name

--- app/routers/segments.py
+++ app/routers/segments.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, status, Depends
 from sqlmodel import Session
+
+from app.schemas.segments import SegmentUpdate
 from ..database.database import get_session
 
 from ..database import segments_crud as crud
@@ -15,10 +17,14 @@ def get_all_segments(*, session: Session = Depends(get_session)):
 def get_segment_by_id(*, session: Session = Depends(get_session), segment_id: int):
     return crud.get_segment_by_id(session, segment_id)
 
-@router.post("", status_code=status.HTTP_201_CREATED, response_model=SegmentDb)
+@router.post('', status_code=status.HTTP_201_CREATED, response_model=SegmentDb)
 def create_segment(*, session: Session = Depends(get_session), segment_in: SegmentIn):
     return crud.create_segment(session, segment_in)
 
-@router.delete("/{segment_id}", response_model=SegmentDb)
+@router.patch('/{segment_id}', response_model=SegmentDb)
+def update_segment_by_id(*, session: Session = Depends(get_session), segment_id:int, segment_update: SegmentUpdate):
+    return crud.update_segment_by_id(session, segment_id, segment_update)
+
+@router.delete('/{segment_id}', response_model=SegmentDb)
 def delete_segment_by_id(*, session: Session = Depends(get_session), segment_id: int):
     return crud.delete_segment_by_id(session, segment_id)
