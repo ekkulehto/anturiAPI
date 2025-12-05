6986aa0bde6c7553173ea1a7990e096bd9fd61e4
Ekku Lehto <ekkulehto@proton.me>
2025-12-05 14:55:22 +0200

    feat: Change sensor tags, update response models and return response 204 from delete without response body

--- app/routers/sensors.py
+++ app/routers/sensors.py
@@ -10,7 +10,7 @@ from ..database.database import get_session
 from ..database import sensors_crud as crud
 from ..database.models import SensorIn, SensorDb, SensorOut, SensorOutWithMeasurements, SensorOutWithStatusHistory, SensorStatus, SensorStatusOut
 
-router = APIRouter(prefix='/sensors', tags=['sensors'])
+router = APIRouter(prefix='/sensors', tags=['Sensors'])
 
 @router.get('', response_model=list[SensorOut])
 def get_all_sensors(*, session: Session = Depends(get_session), sensor_status: SensorStatus | None = Query(
@@ -30,11 +30,11 @@ def get_sensor_status_history_by_id(*, session: Session = Depends(get_session), 
 )):
     return crud.get_sensor_status_history_by_id(session, sensor_id, sensor_status)
 
-@router.post('/{sensor_id}/status', response_model=SensorStatusOut, status_code=status.HTTP_202_ACCEPTED)
+@router.post('/{sensor_id}/status', response_model=SensorOut, status_code=status.HTTP_202_ACCEPTED)
 def change_sensor_status(*, session: Session = Depends(get_session), sensor_id: int, sensor_status_update: SensorStatusUpdate):
     return crud.change_sensor_status(session, sensor_id, sensor_status_update)
 
-@router.post('', status_code=status.HTTP_201_CREATED, response_model=SensorDb)
+@router.post('', status_code=status.HTTP_201_CREATED, response_model=SensorOut)
 def create_sensor(*, session: Session = Depends(get_session), sensor_in: SensorIn):
     return crud.create_sensor(session, sensor_in)
 
@@ -42,6 +42,6 @@ def create_sensor(*, session: Session = Depends(get_session), sensor_in: SensorI
 def update_sensor_by_id(*, session: Session = Depends(get_session), sensor_id: int, sensor_update: SensorUpdate):
     return crud.update_sensor_by_id(session, sensor_id, sensor_update)
 
-@router.delete('/{sensor_id}', response_model=SensorDb)
+@router.delete('/{sensor_id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_sensor_by_id(*, session: Session = Depends(get_session), sensor_id: int):
     return crud.delete_sensor_by_id(session, sensor_id)
