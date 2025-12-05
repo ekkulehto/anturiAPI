009502c7ead2abb0bf8b6e442d345f4f7caea5ac
Ekku Lehto <ekkulehto@proton.me>
2025-12-05 15:17:48 +0200

    style: Reorder measurements CRUD and router

--- app/database/measurements_crud.py
+++ app/database/measurements_crud.py
@@ -2,17 +2,6 @@ from fastapi import HTTPException, Response, status
 from sqlmodel import Session, select
 from .models import MeasurementIn, MeasurementDb, MeasurementOut, MeasurementOutWithSensor
 
-def create_measurement(session: Session, measurement_in: MeasurementIn):
-    measurement = MeasurementDb.model_validate(measurement_in)
-    session.add(measurement)
-    session.commit()
-    session.refresh(measurement)
-
-    return MeasurementOutWithSensor(
-        sensor_id=measurement.sensor_id,
-        measurement=MeasurementOut.model_validate(measurement)
-        )
-
 def get_all_measurements(session: Session):
     measurements_db = session.exec(select(MeasurementDb)).all()
 
@@ -23,6 +12,17 @@ def get_all_measurements(session: Session):
         )
         for measurement in measurements_db
     ]
+
+def create_measurement(session: Session, measurement_in: MeasurementIn):
+    measurement = MeasurementDb.model_validate(measurement_in)
+    session.add(measurement)
+    session.commit()
+    session.refresh(measurement)
+
+    return MeasurementOutWithSensor(
+        sensor_id=measurement.sensor_id,
+        measurement=MeasurementOut.model_validate(measurement)
+        )
 
 def get_measurement_by_id(session: Session, measurement_id: int):
     measurement = session.get(MeasurementDb, measurement_id)
--- app/routers/measurements.py
+++ app/routers/measurements.py
@@ -11,13 +11,13 @@ router = APIRouter(prefix='/measurements', tags=['Measurements'])
 def get_all_measurements(*, session: Session = Depends(get_session)):
     return crud.get_all_measurements(session)
 
-@router.get('/{measurement_id}', response_model=MeasurementOutWithSensor)
-def get_measurement_by_id(*, session: Session = Depends(get_session), measurement_id: int):
-    return crud.get_measurement_by_id(session, measurement_id)
-
 @router.post('', status_code=status.HTTP_201_CREATED, response_model=MeasurementOutWithSensor)
 def create_measurement(*, session: Session = Depends(get_session), measurement_in: MeasurementIn):
     return crud.create_measurement(session, measurement_in)
+
+@router.get('/{measurement_id}', response_model=MeasurementOutWithSensor)
+def get_measurement_by_id(*, session: Session = Depends(get_session), measurement_id: int):
+    return crud.get_measurement_by_id(session, measurement_id)
 
 @router.delete('/{measurement_id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_measurement_by_id(*, session: Session = Depends(get_session), measurement_id: int):
