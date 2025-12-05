5e780505abb33c29e576cd349ebc394b37b20e62
Ekku Lehto <ekkulehto@proton.me>
2025-12-05 14:20:44 +0200

    feat: Change measurement response-model and remove the status-code 204 return from delete

--- app/database/measurements_crud.py
+++ app/database/measurements_crud.py
@@ -1,16 +1,28 @@
 from fastapi import HTTPException, Response, status
 from sqlmodel import Session, select
-from .models import MeasurementIn, MeasurementDb
+from .models import MeasurementIn, MeasurementDb, MeasurementOut, MeasurementOutWithSensor
 
 def create_measurement(session: Session, measurement_in: MeasurementIn):
     measurement = MeasurementDb.model_validate(measurement_in)
     session.add(measurement)
     session.commit()
     session.refresh(measurement)
-    return measurement
+
+    return MeasurementOutWithSensor(
+        sensor_id=measurement.sensor_id,
+        measurement=MeasurementOut.model_validate(measurement)
+        )
 
 def get_all_measurements(session: Session):
-    return session.exec(select(MeasurementDb)).all()
+    measurements_db = session.exec(select(MeasurementDb)).all()
+
+    return [
+        MeasurementOutWithSensor(
+            sensor_id=measurement.sensor_id,
+            measurement=MeasurementOut.model_validate(measurement)
+        )
+        for measurement in measurements_db
+    ]
 
 def get_measurement_by_id(session: Session, measurement_id: int):
     measurement = session.get(MeasurementDb, measurement_id)
@@ -21,7 +33,10 @@ def get_measurement_by_id(session: Session, measurement_id: int):
             status_code=status.HTTP_404_NOT_FOUND
         )
     
-    return measurement
+    return MeasurementOutWithSensor(
+        sensor_id=measurement.sensor_id,
+        measurement=MeasurementOut.model_validate(measurement)
+        )
 
 def delete_measurement_by_id(session: Session, measurement_id: int):
     measurement = session.get(MeasurementDb, measurement_id)
@@ -34,4 +49,3 @@ def delete_measurement_by_id(session: Session, measurement_id: int):
     
     session.delete(measurement)
     session.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
