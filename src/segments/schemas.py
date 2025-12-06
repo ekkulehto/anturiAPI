ee3d0afe4e6db46688757a29148e6740a9b97ca1
Ekku Lehto <ekkulehto@proton.me>
2025-12-06 15:07:19 +0200

    docs: Update descriptions

--- src/segments/schemas.py
+++ src/segments/schemas.py
@@ -2,4 +2,4 @@ from typing import Optional
 from sqlmodel import Field, SQLModel
 
 class SegmentUpdate(SQLModel):
-    name: Optional[str] = Field(default=None, description='New Segment name')
+    name: Optional[str] = Field(default=None, description='New name for the segment. If omitted, the name is not changed.')
