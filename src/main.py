51e9a9eb674ec59a6b0722e4da253a1e5d4fbfa2
Ekku Lehto <ekkulehto@proton.me>
2025-12-08 18:39:16 +0200

    feat: Add sensor status as it's own router

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 
 from .database import create_db
 from .sensors.router import router as sensors_router
+from .sensor_status.router import router as sensor_status_router
 from .measurements.router import router as measurements_router
 from .segments.router import router as segments_router
 
@@ -13,6 +14,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-app.include_router(sensors_router)
-app.include_router(measurements_router)
 app.include_router(segments_router)
+app.include_router(sensors_router)
+app.include_router(sensor_status_router)
+app.include_router(measurements_router)
